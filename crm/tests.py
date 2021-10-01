c8edb54ecd5ab36a6dc9bc8549267c57ed8848d0
mispelledname <joanne.ngzy.is@gmail.com>
2021-10-01 23:06:04 +1000

    add imports

--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpTest(TestCase):
+    def test_create_user_account(self):
+        url = reverse('useraccounts')
+        
