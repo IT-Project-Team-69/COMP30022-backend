94cfcc3a60ee843191c45a9d67804ccdbac9a93e
mispelledname <joanne.ngzy.is@gmail.com>
2021-10-01 21:00:44 +1000

    add backend wsgi to procfile

--- backend/settings.py
+++ backend/settings.py
@@ -143,5 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Where frontend and backend connect 
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000'
 ]
